dd66b021d38c7429bac56e9e928d089ed3fabb75
Yurii Bazhan <yuriibazhan@gmail.com>
2022-07-25 10:32:56 +0300

    Fixing second home task

--- src/test/java/homework2/NumberHolderTest.java
+++ src/test/java/homework2/NumberHolderTest.java
@@ -2,20 +2,20 @@ package homework2;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class NumberHolderTest {
 
     @Test
-    public void testConstructor(){
-        NumberHolder box = new NumberHolder(10,10.1F);
-        NumberHolder plate = new NumberHolder(5,5.1F);
+    public void testConstructor() {
+        NumberHolder box = new NumberHolder(10, 10.1F);
+        NumberHolder plate = new NumberHolder(5, 5.1F);
         NumberHolder bucket = new NumberHolder();
 
-        assertEquals(10, box.getAnInt(),"Incorrect anInt variable");
-        assertEquals(5.1F, plate.getaFloat(),"Incorrect aFloat variable");
+        assertEquals(10, box.getAnInt(), "Incorrect anInt variable");
+        assertEquals(5.1F, plate.getaFloat(), "Incorrect aFloat variable");
 
         bucket.setAnInt(10);
-        assertEquals(10,bucket.getAnInt(),"Incorrect anInt variable");
+        assertEquals(10, bucket.getAnInt(), "Incorrect anInt variable");
     }
 }
